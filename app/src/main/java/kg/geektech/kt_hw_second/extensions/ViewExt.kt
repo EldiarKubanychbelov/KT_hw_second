922d5c638c30505ac84a6e713ecdd7f0e8019b84
eldiar644@gmail.com <eldiar6442003@gmail.com>
2021-09-18 15:50:29 +0600

    Initial commit

---
+++ app/src/main/java/kg/geektech/kt_hw_second/extensions/ViewExt.kt
@@ -0,0 +1,9 @@
+package space.geektech.kotlinlesson2.extension
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun ImageView.loadImageView(context: Context, url: String){
+    Glide.with(context).load(url).into(this)
+}
---
+++ app/src/main/java/kg/geektech/kt_hw_second/model/Member.kt
@@ -0,0 +1,8 @@
+package kg.geektech.kt_hw_second.model
+
+data class Member (
+    var name: String? = null,
+    var phone: String? = null,
+    var isFriend: Boolean? = null
+)
+
